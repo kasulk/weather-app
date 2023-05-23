657f5c1f44de6e6d4e674f5d9990143b252830d8
kasi <danielkaser@mailbox.org>
2023-05-23 10:15:30 +0200

    pair switch places

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,28 +1,46 @@
+import { useState } from "react";
 import useFetch from "../../utils/useFetch";
 
-export default function Form() {
-  function handleSubmit() {}
+export default function Form({ onAddActivity }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const name = event.target.activity.value;
+    // console.log("form:", form);
+    const isForGoodWeather = event.target.isGood.checked;
+    // console.log("isForGoodWeather:", isForGoodWeather);
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log("data:", data);
+    const dataBla = {};
+  }
 
   function Weather() {
     const weather = JSON.stringify(
       useFetch("https://example-apis.vercel.app/api/weather/")
     );
 
-    console.log("Weather:", weather);
+    // console.log("Weather:", weather);
   }
   Weather();
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Add new activity:</h1>
         <label htmlFor="activity">Name: </label>
-        <input type="text" name="activity" id="activity" />
+        <input
+          type="text"
+          name="activity"
+          id="activity"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
         <label htmlFor="is-good">Good-weather activity </label>
-        <input type="checkbox" name="is-good" id="is-good" />
-        <button type="submit" onSubmit={handleSubmit}>
-          Submit
-        </button>
+        <input type="checkbox" name="isGood" id="is-good" />
+        <button type="submit">Submit</button>
       </form>
     </>
   );
