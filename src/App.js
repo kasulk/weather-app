4d0214212712c9c8060b8ce2fbedf31f6eddc8a1
kasi <danielkaser@mailbox.org>
2023-05-23 11:42:33 +0200

    list component renders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import Form from "./components/Form";
 import { useState } from "react";
+import List from "./components/List";
 
 function App() {
   const [activities, setActivities] = useState([]);
 
   function handleAddActivity(activity) {
-
-    setActivities([...activities, activity])
+    setActivities([...activities, activity]);
     console.log(activities);
-
   }
   return (
     <div className="App">
+      <ul>
+        <List activities={activities} />
+      </ul>
       <Form onAddActivity={(activity) => handleAddActivity(activity)} />
     </div>
   );
---
+++ src/components/List/index.js
@@ -0,0 +1,9 @@
+export default function List({ activities }) {
+  return (
+    // <>
+    activities.map((activity) => {
+      return <li key={activity.id}>{activity.name}</li>;
+    })
+    // </>
+  );
+}
