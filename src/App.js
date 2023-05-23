c03a68afd11cd9e3cdd1239a06cdc8423fffac64
kasi <danielkaser@mailbox.org>
2023-05-23 12:43:04 +0200

    activity filter works

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import List from "./components/List";
 import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [activities, setActivities] = useLocalStorageState("activities", { defaultValue: null });
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: dummyData,
+  });
+  // const [filteredActivities, setFilteredActivities] = useState([]);
+  let filteredActivities = [];
+  const isGoodWeather = true;
   // const [activities, setActivities] = useState([]);
-
 
   function handleAddActivity(activity) {
     setActivities([...activities, activity]);
     console.log(activities);
   }
+
+  filteredActivities = activities.filter(
+    (activity) => activity.isForGoodWeather === isGoodWeather
+  );
+  // );
+
   return (
     <div className="App">
       <ul>
-        <List activities={activities} />
+        <List
+          activities={activities}
+          filteredActivities={filteredActivities}
+          isGoodWeather={isGoodWeather}
+        />
       </ul>
       <Form onAddActivity={(activity) => handleAddActivity(activity)} />
     </div>
@@ -24,3 +38,11 @@ function App() {
 }
 
 export default App;
+
+const dummyData = [
+  {
+    id: 0,
+    name: "Kiss girlfried",
+    isForGoodWeather: true,
+  },
+];
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,9 +1,16 @@
-export default function List({ activities }) {
+export default function List({
+  activities,
+  isGoodWeather,
+  filteredActivities,
+}) {
   return (
     // <>
-    activities.map((activity) => {
-      return <li key={activity.id}>{activity.name}</li>;
-    })
+    // activities
+    filteredActivities
+      //   .filter((activity) => activity.isForGoodWeather === isGoodWeather)
+      .map((activity) => {
+        return <li key={activity.id}>{activity.name}</li>;
+      })
     // </>
   );
 }
