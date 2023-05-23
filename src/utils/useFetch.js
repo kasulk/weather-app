4a5dd6b35fa640b5b59d264a3bd72581c99255ca
CtzarGM <cesarga4649@google.com>
2023-05-23 16:20:24 +0200

    clean useFetch.js

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -1,22 +1,5 @@
 import { useEffect, useState } from "react";
 
-/* export default function useFetch(url) {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    async function startFetching() {
-      const response = await fetch(url);
-      const newData = await response.json();
-
-      setData(newData);
-    }
-    startFetching();
-  }, []);
-
-  return data;
-}
- */
-
 export default function useFetch(url) {
   const [data, setData] = useState();
 
